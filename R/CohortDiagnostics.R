2b28ef74e37f99d92eeb9b49f3a993898ac3c192
UMJI won <jiwontonum@gmail.com>
2025-08-11 13:19:11 +0900

    Update CohortDiagnostics.R
    outlined Temporal CohortCharacterization

--- R/CohortDiagnostics.R
+++ R/CohortDiagnostics.R
@@ -203,19 +203,17 @@ execute <- function(connectionDetails,
                                                                    addDescendantsToExclude = FALSE,
                                                                    includedCovariateIds = c()
                                                                    ),
-    runTemporalCohortCharacterization = TRUE,
-    temporalCovariateSettings = FeatureExtraction::createTemporalCovariateSettings(
-      useConditionOccurrence =
-        TRUE,
-      useDrugEraStart = TRUE,
-      useProcedureOccurrence = TRUE,
-      useMeasurement = TRUE,
-      temporalStartDays = c(-365, -30, 0, 1, 31),
-      temporalEndDays = c(-31, -1, 0, 30, 365)
-    ),
-    minCellCount = 5,
-    incremental = TRUE,
-    incrementalFolder = incrementalFolder
+    #runTemporalCohortCharacterization = TRUE,
+    #temporalCovariateSettings = FeatureExtraction::createTemporalCovariateSettings(
+      #useConditionOccurrence = TRUE,
+      #useDrugEraStart = TRUE,
+      #useProcedureOccurrence = TRUE,
+      #useMeasurement = TRUE,
+      #temporalStartDays = c(-365, -30, 0, 1, 31),
+      #temporalEndDays = c(-31, -1, 0, 30, 365)),
+    #minCellCount = 5,
+    #incremental = TRUE,
+    #incrementalFolder = incrementalFolder
   )
 
   # drop cohort stats table
@@ -226,3 +224,4 @@ execute <- function(connectionDetails,
     connection = NULL
   )
 }
+
